36b5801bbd6b4271940c54673efcf9e8959fdff6
Amelia Manders <amelia.manders@suny.edu>
2015-03-10 14:46:40 -0400

    course filters

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,43 @@
 class CoursesController < InheritedResources::Base
 
+
+
+  def index
+      @filterrific = initialize_filterrific(
+          Course,
+          params[:filterrific],
+   select_options: {
+            sorted_by: Course.options_for_sorted_by,
+           with_school_id: School.options_for_select
+         }#,
+  #        persistence_id: 'shared_key',
+  #              default_filter_params: {},
+  #              available_filters: [],
+                    ) or return
+                    @courses = @filterrific.find.page(params[:page])
+                        # Respond to html for initial page load and to js for AJAX filter updates.
+                        respond_to do |format|
+                          format.html
+                          format.js
+                        end
+
+                      # Recover from invalid param sets, e.g., when a filter refers to the
+                      # database id of a record that doesn’t exist any more.
+                      # In this case we reset filterrific and discard all filter params.
+                    #  rescue ActiveRecord::RecordNotFound => e
+                        # There is an issue with the persisted param_set. Reset it.
+                    #    puts "Had to reset filterrific params: #{ e.message }"
+                     #   redirect_to(reset_filterrific_url(format: :html)) and return  
+    end
+
+
+
+
+
+
+
+
+
   private
 
     def course_params
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,96 @@ class Course < ActiveRecord::Base
   belongs_to :school, :foreign_key => 'school_id', :class_name => "School"
   belongs_to :catalog, :foreign_key => 'catalog_id', :class_name => "Catalog"
   
-  
+  filterrific default_filter_params: { :sorted_by => 'title_asc' },
+               available_filters: [
+                 :sorted_by,
+                 :search_query,
+                 :with_school_id
+               ]
+
+   # default for will_paginate
+   #self.per_page = 10
+
+   scope :search_query, ->(query){ 
+     return nil  if query.blank?
+     # condition query, parse into individual keywords
+     terms = query.downcase.split(/\s+/)
+     # replace "*" with "%" for wildcard searches,
+     # append '%', remove duplicate '%'s
+     terms = terms.map { |e|
+       (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+     }
+     # configure number of OR conditions for provision
+     # of interpolation arguments. Adjust this if you
+     # change the number of OR conditions.
+     num_or_conditions = 1
+
+     where(
+       terms.map {
+         or_clauses = [
+           "LOWER(courses.title) LIKE ?"
+         ].join(' OR ')
+         "(#{ or_clauses })"
+       }.join(' AND '),
+       *terms.map { |e| [e] * num_or_conditions }.flatten
+     )
+   }
+
+   scope :sorted_by, lambda { |sort_option|
+     # extract the sort direction from the param value.
+     direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+     case sort_option.to_s
+     when /^title_/
+       order("courses.title #{ direction }")
+     when /^level_/
+       order("LOWER(programs.level) #{ direction }")
+     else
+       raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+     end
+   }
+   scope :with_school_id, lambda { |school_ids|
+     where(:school_id => [*school_ids])
+   }
+   scope :with_created_at_gte, lambda { |ref_date|
+     where('points_transactions.created_at >= ?', ref_date)
+   }
+
+
+   def self.options_for_sorted_by
+     [
+       ['Registration date (newest first)', 'title_desc'],
+       ['Registration date (oldest first)', 'title_asc'],
+     ]
+   end
+
+
+
+
+ #  scope :search_query, lambda { |query|
+ #    return nil  if query.blank?
+ #    # condition query, parse into individual keywords
+ #    terms = query.downcase.split(/\s+/)
+ #    # replace "*" with "%" for wildcard searches,
+ #    # append '%', remove duplicate '%'s
+ #    terms = terms.map { |e|
+ #      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+ #    }
+ #    # configure number of OR conditions for provision
+ #    # of interpolation arguments. Adjust this if you
+ #    # change the number of OR conditions.
+ #    num_or_conditions = 3
+ #    where(
+ #      terms.map {
+ #        or_clauses = [
+ #          "LOWER(students.first_name) LIKE ?",
+ #          "LOWER(students.last_name) LIKE ?",
+ #          "LOWER(students.email) LIKE ?"
+ #        ].join(' OR ')
+ #        "(#{ or_clauses })"
+ #      }.join(' AND '),
+ #      *terms.map { |e| [e] * num_or_conditions }.flatten
+ #    )
+ #  }
   
   
   
