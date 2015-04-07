df3dcd7494cc7fd82da6260e5bd313b25270bb88
Amelia Manders <amelia.manders@suny.edu>
2015-04-07 12:50:36 -0400

    adding js to fix dropdowns

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,11 +59,9 @@ Rails.application.configure do
   #AWS S3 config
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    :bucket         => 'navigator-storage',
+    :path           => ':attachment/:id/:style.:extension'
   }
   
   
