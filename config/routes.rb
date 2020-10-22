1099bb597f3a6fbf9fea67ea424f44893e28ca3b
amanders23 <amelia.manders@suny.edu>
2020-10-22 09:55:18 -0400

    push new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
 
   #devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'visitors#index'
+  #root to: 'visitors#index'
+  root to: 'courses#index'
   #devise_for :users
   
   
