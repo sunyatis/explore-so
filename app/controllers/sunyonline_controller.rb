2e97eb671daf4e30e2f3fb69dbf15b2261e985dc
amanders23 <amelia.manders@suny.edu>
2021-01-22 11:57:08 -0500

    sunyonline files

--- app/controllers/sunyonline_controller.rb
+++ app/controllers/sunyonline_controller.rb
@@ -18,25 +18,29 @@ class SunyonlineController < ApplicationController
 #   end
   
     def show
+      if (params.has_key?(:page3))
+        puts "#{params[:page3]}"
+            render template: "sunyonline/#{params[:page3]}/#{params[:page2]}/#{params[:page1]}/#{params[:page]}"
+         else
+             end
+           
+           if (params.has_key?(:page2))
+             puts "#{params[:page2]}"
+                 render template: "sunyonline/#{params[:page2]}/#{params[:page1]}/#{params[:page]}"
+              else
+            
+               end
       if (params.has_key?(:page1))
         puts "#{params[:page1]}"
             render template: "sunyonline/#{params[:page1]}/#{params[:page]}"
          else
             
           end
-          if (params.has_key?(:page2))
-            puts "#{params[:page2]}"
-                render template: "sunyonline/#{params[:page2]}/#{params[:page1]}/#{params[:page]}"
-             else
-            
-              end
+          
               
-              if (params.has_key?(:page3))
-                puts "#{params[:page3]}"
-                    render template: "sunyonline/#{params[:page3]}/#{params[:page2]}/#{params[:page1]}/#{params[:page]}"
-                 else
+              
             
-                  end
+                
       
        # render template: "sunyonline/#{params[:page]}"
     end
