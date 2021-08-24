27dabf2765612042c0741265e3c96376d3791486
amanders23 <amelia.manders@suny.edu>
2021-08-24 09:00:50 -0400

    sunyonline

--- app/controllers/pathways_controller.rb
+++ app/controllers/pathways_controller.rb
@@ -6,17 +6,11 @@ class PathwaysController < ApplicationController
   end
 
 def accounting 
+  @pathways = Pathway.where(:landing_page => 'accounting').uniq
   render 'accounting', layout: false
   #@programs = Program.where(:open_suny => 'Open SUNY').uniq
 end
-def accounting1 
-  render 'accounting1', layout: false
-  #@programs = Program.where(:open_suny => 'Open SUNY').uniq
-end
-def accounting1 
-  render 'accounting1', layout: false
-  #@programs = Program.where(:open_suny => 'Open SUNY').uniq
-end
+
 def business
   @pathways = Pathway.where(:landing_page => 'business').uniq
   render 'business', layout: false
@@ -24,37 +18,47 @@ def business
 end
 
 def computerscience_it
+  @pathways = Pathway.where(:landing_page => 'computerscience_it').uniq
   render 'computerscience_it', layout: false
   #@programs = Program.where(:open_suny => 'Open SUNY').uniq
 end
 
 def criminal_justice
+   @pathways = Pathway.where(:landing_page => 'criminal_justice').uniq
   render 'criminal_justice', layout: false
   #@programs = Program.where(:open_suny => 'Open SUNY').uniq
 end
 def data_science
+   @pathways = Pathway.where(:landing_page => 'data_science').uniq
   render 'data_science', layout: false
 end
 def education
+   @pathways = Pathway.where(:landing_page => 'education').uniq
   render 'education', layout: false
 end
 def engineering
+   @pathways = Pathway.where(:landing_page => 'engineering').uniq
   render 'engineering', layout: false
 end
 
 def general_studies
+   @pathways = Pathway.where(:landing_page => 'general_studies').uniq
   render 'general_studies', layout: false
 end
 def graduate_degrees
+   @pathways = Pathway.where(:landing_page => 'graduate_degrees').uniq
   render 'graduate_degrees', layout: false
 end
 def healthcare
+   @pathways = Pathway.where(:landing_page => 'healthcare').uniq
   render 'healthcare', layout: false
 end
 def psychology
+   @pathways = Pathway.where(:landing_page => 'psychology').uniq
   render 'psychology', layout: false
 end
 def security_studies
+   @pathways = Pathway.where(:landing_page => 'security_studies').uniq
   render 'security_studies', layout: false
 end
 
