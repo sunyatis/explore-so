6c5d1dced4197035010ed8e88138c66d14efbe1a
amanders23 <amelia.manders@suny.edu>
2021-10-21 11:32:31 -0400

    sunyonline

--- public/assets/application-12d25bb31464e2f629236bae5beaffe6e7b78499affe446eaa61ca16da228fff.js
+++ public/assets/application-12d25bb31464e2f629236bae5beaffe6e7b78499affe446eaa61ca16da228fff.js
@@ -23945,14 +23945,7 @@ function myFunction() {
  
 	});
 	
-	$('#owl-demo3').owlCarousel({
-	    loop: true,
-	    margin: 30,
-	    dots: true,
-	    nav: true,
-	    items: 5,
-	})
-	
+
 	
 	$(function(){
 	  $('#Container').on('mixLoad', function() {
