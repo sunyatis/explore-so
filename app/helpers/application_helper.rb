52faada27e8602709dba4a3aad5571bfc413a998
amanders23 <amelia.manders@suny.edu>
2021-11-01 12:32:31 -0400

    sunyonline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     content_for(:meta_key) {keywords}
   end
   
+  def meta_desc(description)
+    content_for(:meta_desc) {description}
+  end
+  
   def yield_or_default(section, default = "")
       content_for?(section) ? content_for(section) : default
   end
