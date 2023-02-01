e134f1313af71d1aef292e46746e63b419b95980
amanders23 <amelia.manders@suny.edu>
2023-02-01 08:22:33 -0500

    cookies

--- public/assets/application-6323c16e9abcb0c165b1104e7e4f91484761dd6351a70117745c547be4e965b3.js
+++ public/assets/application-6323c16e9abcb0c165b1104e7e4f91484761dd6351a70117745c547be4e965b3.js
@@ -23843,6 +23843,57 @@ function myFunction() {
 	  
 	}
 	
+	function myFunction2() {
+
+	  var ctext = document.getElementById('c').value
+		  if (ctext=="")
+		  {
+		  	window.location =  "/courses?" + "filterrific[search_query]=" + ctext;
+		  }else
+		  window.location = "/courses?" + "filterrific[search_query]=" + ctext;
+	  
+		}
+	
+	
+  function toggleDiv(divid)
+    {
+ 
+      varon = divid + 'on';
+      varoff = divid + 'off';
+ 
+      if(document.getElementById(varoff).style.display == 'block')
+      {
+      document.getElementById(varoff).style.display = 'none';
+      document.getElementById(varon).style.display = 'block';
+	  var v = document.getElementById("progbutton");
+	         v.className += "btn-pressed";
+			 
+	  var element = document.getElementById("courbutton");
+	  element.classList.remove("btn-pressed");
+      }
+   
+
+  } 
+	
+function toggleDiv2(divid)
+  {
+
+    varon = divid + 'on';
+    varoff = divid + 'off';
+
+    if(document.getElementById(varon).style.display == 'block')
+    {
+    document.getElementById(varon).style.display = 'none';
+    document.getElementById(varoff).style.display = 'block';
+    var v = document.getElementById("courbutton");
+         v.className += "btn-pressed";
+   	  var element = document.getElementById("progbutton");
+   	  element.classList.remove("btn-pressed");
+
+    }
+}
+
+
 
 
 	
@@ -24080,4 +24131,67 @@ function myFunction() {
 	});
 	
 	
+	//cookie 
 	
+	class CookieBar {
+	  constructor() {
+	    this.cookiesBar = document.getElementById('cookies-bar');
+	  }
+
+	  init() {
+	    if (this.cookiesAllowed()) {
+	      this.appendGACode();
+	    }
+
+	    this.addButtonBehaviors();
+	  }
+
+	  cookiesAllowed() {
+	    return Cookies.get('allow_cookies') === 'yes';
+	  }
+
+	  addButtonBehaviors() {
+	    if (!this.cookiesBar) {
+	      return;
+	    }
+
+	    this.cookiesBar.querySelector('.accept').addEventListener('click', () => this.allowCookies(true));
+
+	    this.cookiesBar.querySelector('.reject').addEventListener('click', () => this.allowCookies(false));
+	  }
+
+	  appendGACode() {
+	    const ga = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
+	      "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
+	      "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
+	      "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
+	      "ga('create', 'UA-XXXXX-Y', 'auto');" + "ga('send', 'pageview');";
+
+	    document.getElementsByTagName('head')[0].append('<script>' + ga + '</script>');
+	  }
+
+	  allowCookies(allow) {
+	    if (allow) {
+	      Cookies.set('allow_cookies', 'yes', {
+	        expires: 365
+	      });
+
+	      this.appendGACode();
+	    } else {
+	      Cookies.set('allow_cookies', 'no', {
+	        expires: 365
+	      });
+	    }
+
+	    this.cookiesBar.classList.add('hidden');
+	  }
+	}
+
+	window.onload = function() {
+	  const cookieBar = new CookieBar();
+
+	  cookieBar.init();
+	}
+	
+	
+;
